8b57b2a7113c85dbf5769e0d629c5697fb53ff98
Julien DERACHE <julienderache.pro@gmail.com>
2024-02-08 15:42:25 +0100

    ecosystem:5 - unit testing with Jest

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,14 @@
+// Header.test.tsx
+
+import { render, screen } from "@testing-library/react-native";
+import React from "react";
+import Header from "./Header";
+
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    render(<Header title="SpaceCraft" />);
+
+    screen.getByText("SpaceCraft");
+  });
+});
